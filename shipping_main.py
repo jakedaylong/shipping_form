d83fa5aedf3392ee37a71ba425a44c2d1e53c09a
Jake Daylong <jakedaylong@gmail.com>
2024-08-07 00:06:47 -0700

    removing two unnecessary files

--- shipping_main.py
+++
@@ -1,9 +0,0 @@
-import shipping_form as sf
-import directory_creation as dc
-import button_functions as bf
-import test_file as tf
-from nicegui import ui
-import os
-
-
-
--- test_file.py
+++
@@ -1,6 +0,0 @@
-import shipping_form as sf
-from nicegui.elements.mixins.value_element import ValueElement
-
-
-def print(shipper_form):
-    print(shipper_form)
